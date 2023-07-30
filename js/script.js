c0ea844085b6023ca6f9a40a9833bb09a608fd7f
siyamprince <siyamprince007@gmail.com>
2023-07-30 17:10:01 +0600

    sonarbangla

--- js/script.js
+++ js/script.js
@@ -28,3 +28,61 @@ const navBar = document.querySelector('.navbar');
 window.addEventListener("scroll", function(){
     navBar.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+//============= slider background images
+
+const btns = document.querySelectorAll(".slider-btn");
+const slides = document.querySelectorAll(".video-slide");
+
+var sliderBack = function(menual){
+
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    btns[menual].classList.add("active");
+    slides[menual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderBack(i);
+    });
+});
+
+
+// ======== auto play slider ============
+
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    var repeater = () => {
+        setTimeout(function(){
+
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove("active");
+            });
+
+            slides[i].classList.add("active");
+            btns[i].classList.add("active");
+            i++;
+
+            if(slides.length == i){
+                i = 0;
+            }
+            if(i >= slides.length){
+                return;
+            }
+            repeater();
+        }, 10000);
+    }
+    repeater();
+}
+repeat();
+
