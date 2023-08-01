98b42ec08c3d79f9efbeb9f8134a996c48191825
siyamprince <siyamprince007@gmail.com>
2023-08-01 19:05:24 +0600

    sonarbagla

--- js/script.js
+++ js/script.js
@@ -102,3 +102,22 @@ var swiper = new Swiper(".swiper", {
         prevEl: '.swiper-button-prev',
     },
   });
+
+
+  /*=========== counter =============*/
+
+let valueDisplays = document.querySelectorAll('.counter-number');
+let interval = 10000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+        startValue +=1;
+        valueDisplay.textContent = startValue;
+        if (startValue==endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
